341be3e84da2f529acd7b118f65f9c103fdbea4a
Luis <luis.alves.development@gmail.com>
2022-11-10 15:24:37 +0100

    DB and Model create

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
+        'date_of_bith',
         'email',
         'password',
     ];
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function habits(){
+        return this->hasMany('App\Habit');
+    }
 }
--- database/migrations/2022_11_10_132635_create_daily_goals_table.php
+++ database/migrations/2022_11_10_132635_create_daily_goals_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('daily_goals', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('lastname');
-            $table->string('date_of_bith');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('task_id');
+            $table->string('date');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('daily_goals');
     }
 };
